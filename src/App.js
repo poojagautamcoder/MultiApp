12962fdc761353dbe1c40d38acc97663bd9389cd
pooja gautam <poojagautamet@gmail.com>
2023-02-01 17:59:47 +0530

    my wrong code

--- src/App.js
+++ src/App.js
@@ -2,36 +2,18 @@ import { useState } from "react";
 import "./App.css";
 function App() {
   const[newTodo, setnewTodo] = useState("")
-  const [state, setState] = useState([
-    {
-      name: "pooja",
-      age: "27",
-      class: 5
-    },
-    {
-      name: "sudhanshu",
-      age: "23",
-      class: 6
-    }
-  ]);
-
+  const [state, setState] = useState([]);
   const AddnewTodo = () =>{
     let todo = setState({...state, newTodo})
+    return todo
   }
-
-  const addOnchange = (e) =>{
-    console.log(e, "11111111111111111111")
-    setnewTodo.name(e)
-   
-  }
-
-  console.log(state, "1111111111111111");
+  console.log(state.newTodo, "1111111111111111");
   return (
     <div className="App">
       <input value={newTodo} onChange={e => setnewTodo(e.target.value)}  />
       <button onClick={AddnewTodo}>ADD</button>
       {state.map((data, index) => {
-        return <h1 key={index}>{data.name}</h1>;
+        return <h1 key={index}>{data.newTodo}</h1>;
       })}
     </div>
   );
