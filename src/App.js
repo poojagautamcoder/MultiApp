5d16a3c80e78109fc13296d50685aff6f2c76f86
pooja gautam <poojagautamet@gmail.com>
2023-02-01 17:55:02 +0530

    wrong todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const[newTodo, setnewTodo] = useState("")
+  const [state, setState] = useState([
+    {
+      name: "pooja",
+      age: "27",
+      class: 5
+    },
+    {
+      name: "sudhanshu",
+      age: "23",
+      class: 6
+    }
+  ]);
+
+  const AddnewTodo = () =>{
+    let todo = setState({...state, newTodo})
+  }
+
+  const addOnchange = (e) =>{
+    console.log(e, "11111111111111111111")
+    setnewTodo.name(e)
+   
+  }
+
+  console.log(state, "1111111111111111");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={newTodo} onChange={e => setnewTodo(e.target.value)}  />
+      <button onClick={AddnewTodo}>ADD</button>
+      {state.map((data, index) => {
+        return <h1 key={index}>{data.name}</h1>;
+      })}
     </div>
   );
 }
